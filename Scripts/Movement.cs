38db6105f10a23db67e0f45bb89b506766534e50
WilliamQiufeng <williamqiufeng@outlook.com>
2025-01-22 19:18:04 +0000

    Add sprites and bubble

---
+++ Scripts/Movement.cs
@@ -0,0 +1,21 @@
+namespace Bubble;
+
+using Godot;
+
+public partial class Movement : CharacterBody2D
+{
+    [Export]
+    public int Speed { get; set; } = 100;
+
+    public void GetInput()
+    {
+        var inputDirection = Input.GetVector("left", "right", "up", "down");
+        Velocity = inputDirection * Speed;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        GetInput();
+        MoveAndSlide();
+    }
+}
