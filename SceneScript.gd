77633f675a24043ba35fa7465fe8a3884113e3ef
tz1224 <tz1224@ic.ac.uk>
2025-01-22 19:30:28 +0000

    suicide

---
+++ SceneScript.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("loaded scene")
+
+func _physics_process(delta: float) -> void:
+	pass
--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -4,18 +4,18 @@ using Godot;
 
 public partial class Movement : CharacterBody2D
 {
-    [Export]
-    public int Speed { get; set; } = 100;
+	[Export]
+	public int Speed { get; set; } = 100;
 
-    public void GetInput()
-    {
-        var inputDirection = Input.GetVector("left", "right", "up", "down");
-        Velocity = inputDirection * Speed;
-    }
+	public void GetInput()
+	{
+		var inputDirection = Input.GetVector("left", "right", "up", "down");
+		Velocity = inputDirection * Speed;
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        GetInput();
-        MoveAndSlide();
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		GetInput();
+		MoveAndSlide();
+	}
 }
