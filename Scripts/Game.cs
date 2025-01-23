334a23a7177717502c96a1714a0d475348624f54
tz1224 <tz1224@ic.ac.uk>
2025-01-23 11:11:52 +0000

    moved Game to GameAutoLoad

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -4,35 +4,35 @@ namespace Bubble;
 
 public partial class Game : Node
 {
-    public static Game Instance { get; private set; }
-    public override void _EnterTree()
-    {
-        base._EnterTree();
-        Instance = this;
-    }
+	public static Game Instance { get; private set; }
+	public override void _EnterTree()
+	{
+		base._EnterTree();
+		Instance = this;
+	}
 
-    public override void _ExitTree()
-    {
-        base._ExitTree();
-        Instance = null;
-    }
+	public override void _ExitTree()
+	{
+		base._ExitTree();
+		Instance = null;
+	}
 
-    [Export]
-    public double GameSpeed
-    {
-        get => Engine.TimeScale;
-        set
-        {
-            Engine.TimeScale = value;
-            EmitSignal(SignalName.GameSpeedChange, value);
-        }
-    }
+	[Export]
+	public double GameSpeed
+	{
+		get => Engine.TimeScale;
+		set
+		{
+			Engine.TimeScale = value;
+			EmitSignal(SignalName.GameSpeedChange, value);
+		}
+	}
 
-    [Signal]
-    public delegate void GameSpeedChangeEventHandler(float newSpeed);
+	[Signal]
+	public delegate void GameSpeedChangeEventHandler(float newSpeed);
 
-    public void SetTheWorld(bool active)
-    {
-        GameSpeed = active ? 0.5f : 1;
-    }
+	public void SetTheWorld(bool active)
+	{
+		GameSpeed = active ? 0.5f : 1;
+	}
 }
