cc88e172251fb4efddd4d50ec095a12c8cc691e7
pastrainbow <adamliofficial@gmail.com>
2025-01-25 14:27:10 +0000

    added upgrade banner generation

--- Scripts/Upgrades/UpgradePopupController.gd
+++ Scripts/Upgrades/UpgradePopupController.gd
@@ -2,7 +2,18 @@ extends Window # Use Control if your root is a Control node
 
 # Called when the scene is loaded
 func _ready():
+	var upgrades = [ManaUpgrade.new(), HealthUpgrade.new(), ManaUpgrade.new()]
+	var positions = [Vector2(160, 1), Vector2(336, 0), Vector2(0, 0)]
 	self.borderless = true
+	for i in range(3):
+		var texture_box = TextureButton.new()  # Create a new TextureRect for each upgrade
+		texture_box.texture_normal = load(upgrades[i].texture_path)  # Load the texture using the sprite_path property
+		texture_box.size = Vector2(64, 128)  # Set the size of the box (you can adjust this as needed)
+		texture_box.position = positions[i]
+		texture_box.connect("pressed", upgrades[i].upgrade)
+		# Add the texture box as a child of the current node
+		self.add_child(texture_box)
+	
 	#self.resizable = false
 	# Connect signals for all buttons
 	$ManaBanner.connect("pressed", Callable(self, "_on_button_pressed"))
