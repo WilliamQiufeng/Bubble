238640c587cf6edf6404c651f34d8e905b161159
WilliamQiufeng <williamqiufeng@outlook.com>
2025-01-25 18:50:37 +0000

    make bubbles bouncy

--- Scripts/Bubbles/Bubble.gd
+++ Scripts/Bubbles/Bubble.gd
@@ -3,3 +3,8 @@ extends RigidBody2D
 
 @export var damage: float = 20
 @onready var controller: BubbleController = $CollisionShape2D/Area2D
+
+func _physics_process(delta):
+	var collision = move_and_collide(linear_velocity * delta, true)
+	if collision:
+		linear_velocity = linear_velocity.bounce(collision.get_normal())
