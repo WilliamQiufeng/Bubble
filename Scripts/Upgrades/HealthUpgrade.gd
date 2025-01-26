824bcb99c8332b727e5e0ab7619226bfff30920b
tz1224 <tz1224@ic.ac.uk>
2025-01-26 00:01:54 +0000

    suicide

--- Scripts/Upgrades/HealthUpgrade.gd
+++ Scripts/Upgrades/HealthUpgrade.gd
@@ -2,7 +2,7 @@ class_name HealthUpgrade
 extends Upgrade
 
 func _init() -> void:
-	texture_path = "res://sprites/buff_hp.png"
+	texture_path = "res://sprites/BuffHp.png"
 
 func upgrade():
 	Game.player.max_hp += 20
