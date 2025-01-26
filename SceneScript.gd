5a2051f79a094b02277cd16911967b23c3deb1aa
pastrainbow <adamliofficial@gmail.com>
2025-01-26 15:07:57 +0000

    added death screen

--- SceneScript.gd
+++ SceneScript.gd
@@ -9,8 +9,13 @@ func _ready() -> void:
 		"ELIMINATE ALL ENEMIES","@{name:TUTORIAL}Left click to fire bubble, E to change fire mode",
 		"Press 1/2/3 to select bubble type"])
 	Game.afterTutorial = true
+	Game.player.connect("player_death", display_death_screen)
 	#var upgrade_popup = load("res://UpgradePopup.tscn").instantiate()
 	#add_child(upgrade_popup)
+
+
+func display_death_screen():
+	Scenes.switchScene("res://Death.tscn")
 
 func _physics_process(delta: float) -> void:
 	pass
